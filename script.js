c473692291334270196b2f5029bdf92cc206ae02
Leonardo Maragna <86269559+LeonardoMaragna11@users.noreply.github.com>
2022-04-24 17:03:01 -0300

    commit

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ let shuffleOrder = ()=>{
 
 //acende a próxima cor
 let lightColor = (element, number)=>{
-    time = time * 500;
+    number = number * 500;
     setTimeout(()=>{
         element.classList.add('selected');
     }, number - 250)
@@ -39,7 +39,7 @@ let lightColor = (element, number)=>{
 let checkOrder = ()=>{
     for(let i in clikedOrder){
         if(clikedOrder[i] != order[i]){
-            lose();
+            gameOver();
             break;
         }
     }
@@ -57,7 +57,53 @@ let clique = (color)=>{
 
     setTimeout(()=>{
         createColorElement(color).classList.remove('selected');
-    })
+        checkOrder();
+    }, 250)
 
-    checkOrder();
 }
+
+let createColorElement = (color)=>{
+    if(color == 0){
+        return green;
+    }else if(color == 1){
+        return red;
+    }else if(color == 2){
+        return yellow
+    }else if(color == 3){
+        return blue
+    }
+}
+
+// função para o proximo ivel do jogo
+let nextLevel = ()=>{
+    score++;
+    shuffleOrder();
+}
+
+// função para game over
+let gameOver = ()=>{
+    alert(`Pontuação: ${score}\n Voce perdeu o jogozn CLique em ok para recomeçar`);7
+    order = [];
+    clikedOrder = [];
+
+    playGame();
+}
+
+let playGame = ()=>{
+    alert('Bem vindo')
+    score = 0
+
+    nextLevel()
+}
+
+green.onclick = ()=> clique(0)
+red.onclick = ()=> clique(1)
+yellow.onclick = ()=> clique(2)
+blue.onclick = ()=> clique(3)
+
+playGame()
+
+// green.addEventListener('click', clique(0))
+// red.addEventListener('click', clique(1))
+// yellow.addEventListener('click', clique(2))
+// blue.addEventListener('click', clique(3))
